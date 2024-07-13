88c97f2d4388d562538fe62863b17dbac6dcaf51
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2024-07-13 17:31:34 +0545

    initial files setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
+import Hero from '../components/HomePage/Hero';
 export default function Home() {
   return (
     <div className="h-screen flex items-center justify-center text-9xl">
-      BODY
+      <Hero />
     </div>
   );
 }
